84956505ff5046549f1679f09eb8b1e833011526
tomoyuki irahara <sunnyday20200101@gmail.com>
2021-06-20 13:34:59 +0900

    リアルタイム件数表示

--- main.js
+++ main.js
@@ -10,30 +10,49 @@
 // });
 
 
-async function callApi() {
+// async function callApi() {
 
-    var keyword = new Vue({
-        el: "#form_input",
-        data: {
-            k: "千葉"
-        }
-    });
-}
-callApi();
+//     var keyword = new Vue({
+//         el: "#form_input",
+//         data: {
+//             k: "千葉"
+//         }
+//     });
+// }
+// callApi();
 
+
+// async function search_button() {
+//     var keyword = document.getElementById('search_keyword').value;
+//     console.log(keyword);
+//     var url = base_url + "areaCode=" + areaCode + "&keyword=" + keyword + "&applicationId=" + apiKey
+//     const res = await fetch(url);
+//     const golfcourse = await res.json();
+//     // console.log("keywordの中身は" + keyword.k);
+//     console.log(url);
+//     console.log(golfcourse);
+//     console.log(golfcourse.count);
+//     document.getElementById('api_output').innerHTML = golfcourse.count;
+// };
 const base_url = "https://app.rakuten.co.jp/services/api/Gora/GoraGolfCourseSearch/20170623?format=json&";
 const apiKey = "1083401508369226218";
 const areaCode = "";
 
-async function search_button() {
-    var keyword = document.getElementById('search_keyword').value;
-    console.log(keyword);
-    var url = base_url + "areaCode=" + areaCode + "&keyword=" + keyword + "&applicationId=" + apiKey
-    const res = await fetch(url);
-    const golfcourse = await res.json();
-    // console.log("keywordの中身は" + keyword.k);
-    console.log(url);
-    console.log(golfcourse);
-    console.log(golfcourse.count);
-    document.getElementById('api_output').innerHTML = golfcourse.count;
-};
+var keyword = new Vue({
+    el: '#form_input',
+    methods: {
+        onInput: async function() {
+            var k = document.getElementById('search_keyword').value;
+            console.log(k)
+            var url = base_url + "areaCode=" + areaCode + "&keyword=" + k + "&applicationId=" + apiKey //+ "&page=2"
+            const res = await fetch(url);
+            const golfcourse = await res.json();
+            console.log(url);
+            console.log(golfcourse);
+            console.log(golfcourse.count);
+            console.log(golfcourse.pageCount);
+            console.log(golfcourse.Items[0].Item.golfCourseName)
+            document.getElementById('api_output').innerHTML = golfcourse.count;
+        }
+    }
+});
